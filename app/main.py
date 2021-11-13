e24cb9786431de95c39cfd36d23e8d39b99283ed
liviaspereira <liviaspereira@users.noreply.github.com>
2021-11-12 22:04:30 -0300

    Atualização

--- app/main.py
+++ app/main.py
@@ -1,13 +1,39 @@
-from fastapi import FastAPI
+from typing import Optional
+
+from fastapi import FastAPI, status
+
+from pydantic import BaseModel
+
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = "Padrão"
+    price: float
+    tax: Optional[float] = None
+    id: Optional[int] = None
+
 
 app = FastAPI()
+lista = []
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/items/", status_code=status.HTTP_201_CREATED)
+async def create_item(item: Item):
+    item.id = len(lista)
+    lista.append(item)
+    return item
+
+@app.put("/items/{item_id}", status_code=status.HTTP_201_CREATED)
+def update_item(item: Item, item_id: int):
+    item.id = item_id
+    lista[item_id] = item
+    return item
+    
+@app.get("/items/")
+def list_items():
+    return lista
 
 
-@app.post("/batata/")
-async def batata():
-    return {"message": "batata"}
+
+
