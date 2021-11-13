41cc1a1f5a8afc877e6d89ead372ff000c5b5635
liviaspereira <liviaspereira@users.noreply.github.com>
2021-11-13 18:50:07 -0300

    FastAPI

--- app/main.py
+++ app/main.py
@@ -1,39 +1,60 @@
 from typing import Optional
 
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 
 from pydantic import BaseModel
 
+from sqlmodel import Field, SQLModel, create_engine, Session, select
 
 
-class Item(BaseModel):
+class Item(SQLModel, table=True):
     name: str
     description: Optional[str] = "Padrão"
     price: float
     tax: Optional[float] = None
-    id: Optional[int] = None
+    id: Optional[int] = Field(default=None, primary_key=True)
 
+postgres_url = "postgresql://postgres:123@localhost:5432/postgres"
+engine = create_engine(postgres_url, echo=True)
+SQLModel.metadata.create_all(engine)
 
 app = FastAPI()
-lista = []
+
+
+@app.get("/items/")
+def list_items():
+    with Session(engine) as session:
+        statement = select(Item)
+        results = session.exec(select(Item)).all()
+    return results
 
 
 @app.post("/items/", status_code=status.HTTP_201_CREATED)
 async def create_item(item: Item):
-    item.id = len(lista)
-    lista.append(item)
+    with Session(engine) as session:
+        session.add(item)   
+        session.commit()
+        session.refresh(item)
     return item
+
 
 @app.put("/items/{item_id}", status_code=status.HTTP_201_CREATED)
-def update_item(item: Item, item_id: int):
-    item.id = item_id
-    lista[item_id] = item
+def update_item(name: str, item_id: int):
+    with Session(engine) as session:
+        item = session.exec(select(Item.where(Item.id == item_id))).one()
+        item.name = name
+        session.add(item)
+        session.commit()
+        session.refresh(item)
     return item
-    
-@app.get("/items/")
-def list_items():
-    return lista
 
 
-
-
+@app.delete("/items/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_item(item_id: int):
+    with Session(engine) as session:
+        try:
+            item = session.exec(select(Item).where(Item.id == item_id)).one()
+        except:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Item not found")
+        session.delete(item)
+        session.commit()
